646b1a44f941a724792c67155c0a87fd2d63a171
lbeder <lbeder@users.noreply.github.com>
2022-11-14 23:31:10 +0000

    Initial commit

---
+++ src/merger.ts
@@ -0,0 +1,23 @@
+import Logger from "./logger";
+import fs from "fs";
+
+export interface MergeOptions {
+  inputDir: string;
+  outputDir: string;
+}
+
+export default class Merger {
+  private options: MergeOptions;
+
+  constructor(options: MergeOptions) {
+    this.options = options;
+  }
+
+  public async merge() {
+    const { inputDir, outputDir } = this.options;
+
+    Logger.info(`Merging Google Voice calls from "${inputDir}" to "${outputDir}"...`);
+
+    fs.mkdirSync(outputDir, { recursive: true });
+  }
+}
